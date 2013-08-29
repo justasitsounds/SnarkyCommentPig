59022809ed614a6edceb288e5245007cdaf2372d
plebon <plebon@gmail.com>
2013-08-29 18:36:29 +1000

    Did stuff

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,8 @@
-var bbtweets = require('../data/bbtweetstream.json');
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
 
-  	res.render('index', { title: 'Snarky Pig', remarks: bbtweets });
+  	res.render('index', { title: 'Snarky Pig Index' });
 };
