806989b9998fa78120fa63be1570e3c67b99a1a4
James Prendergast <james.prendergast@gmail.com>
2013-08-29 19:49:57 +1000

    asset route

--- app/app.js
+++ app/app.js
@@ -7,6 +7,7 @@ var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
 var data = require('./routes/data');
+var assets = require('./routes/assets');
 var http = require('http');
 var path = require('path');
 
@@ -32,6 +33,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/data', data.list);
+app.get('/assets', assets.get);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
